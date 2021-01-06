4606e68de4a5b77a9dcd8123151d18bace777af7
camillamelo <64509576+camillamelo@users.noreply.github.com>
2021-01-06 10:46:34 -0300

    update

---
+++ .ipynb_checkpoints/README-checkpoint.md
@@ -0,0 +1,2 @@
+# 2021_wordcloud
+ Word Cloud for 2021 wished from Twitter using TWINT and ElasticSearch. In Portuguese.
--- twitter2021elk.py
+++ twitter2021elk.py
@@ -4,10 +4,8 @@ c.Search = "que em 2021"
 c.Lang = "pt"
 #c.Year = "2020"
 c.Since = "2020-12-31"
-c.Until = "2021-01-02"
+c.Until = "2021-01-01"
 c.Index_tweets = "tweets2021"
 c.Elasticsearch = "http://127.0.0.1:9200/"
-# c.Output = "desejos2021.csv"
-# c.Store_csv = True
-# c.Count = True
+c.Count = True
 twint.run.Search(c)
